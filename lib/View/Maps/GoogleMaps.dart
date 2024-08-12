8a6fd797081b052cbf40dd6c63fa07e0b978e745
Huzaifa atta <huzaifaatta8152@gmail.com>
2024-08-12 15:56:17 +0500

    Making Taxi App

--- lib/View/Maps/GoogleMaps.dart
+++ lib/View/Maps/GoogleMaps.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 class GoogleMaps extends StatefulWidget {
@@ -9,12 +12,22 @@ class GoogleMaps extends StatefulWidget {
 }
 
 class _GoogleMapsState extends State<GoogleMaps> {
+
+  Completer<GoogleMapController> _controller = Completer();
+  static final CameraPosition _kgooglePlex = const CameraPosition(target: LatLng(33.68, 73.085),zoom: 14 );
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMaps(
+      body: GoogleMap(
+          initialCameraPosition:_kgooglePlex,
+        mapType: MapType.hybrid,
+        onMapCreated: (GoogleMapController controller){
+            _controller.complete(controller);
+        },
 
-      )
+      ),
     );
   }
 }
